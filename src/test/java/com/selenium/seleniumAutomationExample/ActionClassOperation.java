b1ec373ed00b102126e53af717b28ac56ce4baee
prabhukrishnan <kprabhucs@gmail.com>
2022-07-14 08:17:57 +0530

    Action class

--- src/test/java/com/selenium/seleniumAutomationExample/ActionClassOperation.java
+++ src/test/java/com/selenium/seleniumAutomationExample/ActionClassOperation.java
@@ -37,7 +37,7 @@ public class ActionClassOperation {
 	{
 		
 		System.out.println("Selenium Action Class Example :");
-		
+		System.out.println("Selenium Action Class Example :");
 		
 
 		WebElement mouseOver =  driver.findElement(By.xpath("//a[@id='nav-link-accountList']"));
